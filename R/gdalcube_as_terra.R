ea1f8789392bcc322862010843ec5125f378381a
Jasper Van doninck <vandoninck.jasper@gmail.com>
2023-12-05 09:16:46 +0100

    created gdalcube_as_terra.R
    replacement of cube2SpatRaster

--- R/gdalcube_as_terra.R
+++ R/gdalcube_as_terra.R
@@ -1,17 +1,16 @@
 #' Data cube to SpatRaster
 #' 
-#' Convert a proxy data cube object to 
+#' Convert a proxy data cube object to a terra SpatRaster object
 #' 
 #' Probably only works with a 3-dimensional data cube, to be checked
 #' 
-#' @importFrom stars st_as_stars
 #' @importFrom terra rast
 #' @param cube data cube object
 #' @returns a SpatRaster object
 #' 
 #' 
-cube2SpatRaster <- function(cube){
-  r <- stars::st_as_stars(cube) |>
+gdalcube_as_terra <- function(cube){
+  r <- st_as_stars.cube(cube) |>
     terra::rast()
   return(r)
 }
