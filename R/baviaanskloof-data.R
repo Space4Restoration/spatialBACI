f6e194ee55c13014210403883e057efe3d829bd9
Jasper Van doninck <vandoninck.jasper@gmail.com>
2023-12-07 10:36:35 +0100

    baviaanskloof bug fix

---
+++ R/baviaanskloof-data.R
@@ -0,0 +1,20 @@
+#' Baviaanskloof revegetation data
+#' 
+#' Data on large scale planting in Baviaanskloof, South Africa. 
+#' 
+#' Data used and described in detail in del Río-Mena et al.
+#' 
+#' @docType data
+#' @usage data(baviaanskloof)
+#' 
+#' @format A SpatVector object
+#' 
+#' @keywords datasets
+#' 
+#' @references del Río-Mena, T., Willemen, L., Vrieling, A., Snoeys, A., Nelson, A., 2021. Long-term assessment of ecosystem services at ecological restoration sites using Landsat time series. PLOS ONE 16, e0243020.
+#' 
+#' @examples
+#' data(baviaanskloof)
+#' plot(baviaanskloof)
+#' 
+"baviaanskloof"
