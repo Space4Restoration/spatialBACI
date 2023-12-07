3c9f07f24739247f5de94e3228054468c3b0c370
Jasper Van doninck <vandoninck.jasper@gmail.com>
2023-12-07 11:26:29 +0100

    update data

--- R/baviaanskloof-data.R
+++ R/baviaanskloof-data.R
@@ -7,14 +7,17 @@
 #' @docType data
 #' @usage data(baviaanskloof)
 #' 
+#' @importFrom terra unwrap
+#' @importFrom terra plot
 #' @format A SpatVector object
 #' 
 #' @keywords datasets
 #' 
-#' @references del Río-Mena, T., Willemen, L., Vrieling, A., Snoeys, A., Nelson, A., 2021. Long-term assessment of ecosystem services at ecological restoration sites using Landsat time series. PLOS ONE 16, e0243020.
+#' @references del Río-Mena, T., Willemen, L., Vrieling, A., Snoeys, A., Nelson, A., 2021. Long-term assessment of ecosystem services at ecological restoration sites using Landsat time series. PLOS ONE 16, e0243020. <https://doi.org/10.1371/journal.pone.0243020>
 #' 
 #' @examples
 #' data(baviaanskloof)
+#' baviaanskloof <- unwrap(baviaanskloof)
 #' plot(baviaanskloof)
 #' 
 "baviaanskloof"
